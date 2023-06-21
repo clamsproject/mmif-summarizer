b00f1e16b9a35edb72943fa62b23fbfca4a7e8f6
Marc Verhagen <marc@cs.brandeis.edu>
2023-06-21 09:18:44 -0400

    Moving metadata for new clams-python.

--- code/app.py
+++ code/app.py
@@ -13,43 +13,17 @@ See README.md for more details.
 
 """
 
-from clams.appmetadata import AppMetadata
 from mmif.serialize import Mmif
-from mmif.vocabulary import DocumentTypes, AnnotationTypes
-from lapps.discriminators import Uri
 
+import metadata
 import summary
 from server import ClamsConsumer, Restifier
-
-
-VERSION = '0.2.0'
-LICENSE = 'Apache 2.0'
-MMIF_VERSION = '0.4.2'
-MMIF_PYTHON_VERSION = '0.4.8'
-CLAMS_PYTHON_VERSION = '0.5.3'
 
 
 class MmifSummarizer(ClamsConsumer):
 
     def _consumermetadata(self):
-        self.metadata = \
-            AppMetadata(
-                identifier="https://apps.clams.ai/mmif-summarizer",
-                url='https://github.com/clamsproject/mmif-summarizer',
-                name="MMIF Summarizer",
-                description="Summarize a MMIF file.",
-                mmif_version=MMIF_VERSION,
-                app_version=VERSION,
-                app_license=LICENSE,
-                analyzer_version=VERSION,
-                analyzer_license=LICENSE)
-        self.metadata.add_input(DocumentTypes.TextDocument, required=False)
-        self.metadata.add_input(AnnotationTypes.TimeFrame, required=False)
-        self.metadata.add_input(AnnotationTypes.BoundingBox, required=False)
-        self.metadata.add_input(AnnotationTypes.Alignment, required=False)
-        self.metadata.add_input(Uri.TOKEN, required=False)
-        self.metadata.add_input(Uri.NE, required=False)
-        return self.metadata
+        return metadata.appmetadata()
 
     def _consume(self, mmif, **kwargs):
         self.mmif = mmif if type(mmif) is Mmif else Mmif(mmif)
